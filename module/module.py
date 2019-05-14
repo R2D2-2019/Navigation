b8d0b81fc22b9791dc76a9323898d0865398b268
nicovanbentum <niico0708@gmail.com>
2019-05-14 18:05:03 +0200

    added node to instruction function

--- module/module.py
+++ module/module.py
@@ -1,36 +1,51 @@
 from client.comm import BaseComm
 from common.frame_enum import FrameType
-
 from .Algorithms import AStar
 from .Structure import Grid
 
-#TODO: make a pull request for this frame type
+# TODO: make a pull request for this frame type
+
+
 class move_instruction_c:
-    def __init__(self, path_id, rotation, distance):
+    def __init__(self, path_id=0, id=0, x=0, y=0):
         self.path_id = path_id
-        self.rotation = rotation
-        self.distance = distance
+        self.instruction_id = id
+        self.x = x
+        self.y = y
 
 #TODO: documentation
-#TODO: Unit tests
-class Navigation:
-    def convert_path_to_instructions(self, path = None):
+# TODO: Unit tests
 
-        #theoretical code for now to get a path, should come together later in release branch
-        test_grid = Grid(5,5)
-        test_algo = AStar(test_grid, test_grid[0,0], test_grid[4,4]) 
+
+class Navigation:
+    def get_path_d(self):
+        # simple function for now to get a path, should come together later in release branch
+        test_grid = Grid(11, 11)
+        robot_start = test_grid[5, 5]
+        destination = test_grid[0, 10]
+        test_algo = AStar(test_grid, robot_start, destination)
+
         if not test_algo.solve():
             print("failed to plot path")
-        print("path: ")
-        print(test_algo.path)
+        return test_algo.path
 
+    def get_instruction_path(self, path, path_id):
+        instruction_path = list()
+        self.path_id += 1
+        instruction_id = 0
 
+        for coord in path:
+            instruction = move_instruction_c(
+                path_id, instruction_id, coord[0], coord[1])
+            instruction_id += 1
+            instruction_path.append(instruction)
+        return instruction_path
 
     def __init__(self, comm: BaseComm):
+        self.path_id = 0
         self.comm = comm
         # we'll need to figure out what frames to listen for
         # self.comm.listen_for([FrameType.ACTIVITY_LED_STATE])
-        self.convert_path_to_instructions()
 
     def process(self):
         while self.comm.has_data():
